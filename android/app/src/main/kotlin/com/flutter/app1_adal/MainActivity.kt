06f2a39addb3bf67784a72e8034cb72a7a4282d0
Adal Emmanuel Asiain Gomez <aasiaingomez@gmail.com>
2024-09-18 14:24:52 -0600

    Commit

---
+++ android/app/src/main/kotlin/com/flutter/app1_adal/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.flutter.app1_adal
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/principal.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(MiApp());
+}
+
+class MiApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
+
+
+class MiappEstado extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    throw UnimplementedError();
+  }
+
+}
+
+class _MiAppEstado extends State<MiappEstado>{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
+
+
